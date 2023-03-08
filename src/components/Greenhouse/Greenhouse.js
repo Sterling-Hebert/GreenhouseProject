9b62eebc3a8410e4914cf829d2894edffaebe9c9
sterling_hebert <sterling_hebert@outlook.com>
2023-03-08 17:51:49 -0600

    second

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,18 +1,17 @@
-import dayImage from './images/greenhouse-day.jpg';
-import nightImage from './images/greenhouse-night.jpg';
-import './Greenhouse.css';
+import dayImage from "./images/greenhouse-day.jpg";
+import nightImage from "./images/greenhouse-night.jpg";
+import "./Greenhouse.css";
 
-import LightSwitch from './LightSwitch';
-import ClimateStats from './ClimateStats';
+import LightSwitch from "./LightSwitch";
+import ClimateStats from "./ClimateStats";
+import { useTheme } from "../../context/ThemeContext";
 
 function Greenhouse() {
-
+  const { themeName } = useTheme();
+  const imageSrc = themeName === "day" ? dayImage : nightImage;
   return (
     <section>
-      <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
-      />
+      <img className="greenhouse-img" src={imageSrc} alt="greenhouse" />
       <LightSwitch />
       <ClimateStats />
     </section>
